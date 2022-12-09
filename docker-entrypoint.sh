c72d2f1c8ffd4306febf5fa1bbeb9f368f7ac039
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-12-09 18:07:45 +0800

    fix ARTIFACTS_DESTINATION

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ BACKEND_STORE_URI=${BACKEND_STORE_URI:-"sqlite:////data/mlflow.db"}
 LISTENING_PORT=${LISTENING_PORT:-5000}
 
 if [ $# -eq 0 ]; then
-  mkdir -p $ARTIFACT_DESTINATION_ROOT
+  mkdir -p $ARTIFACTS_DESTINATION
   mlflow server --host 0.0.0.0 --port ${LISTENING_PORT} \
     --serve-artifacts --artifacts-destination $ARTIFACTS_DESTINATION \
     --backend-store-uri ${BACKEND_STORE_URI}
