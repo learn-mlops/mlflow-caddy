651abe3b482f8d055d74c1933425aaa1ad51062f
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-12-09 17:44:51 +0800

    create dir ARTIFACT_DESTINATION_ROOT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ BACKEND_STORE_URI=${BACKEND_STORE_URI:-"sqlite:////data/mlflow.db"}
 LISTENING_PORT=${LISTENING_PORT:-5000}
 
 if [ $# -eq 0 ]; then
+  mkdir -p $ARTIFACT_DESTINATION_ROOT
   mlflow server --host 0.0.0.0 --port ${LISTENING_PORT} \
     --artifact-destination-root $ARTIFACT_DESTINATION_ROOT \
     --backend-store-uri ${BACKEND_STORE_URI}
