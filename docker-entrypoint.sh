5c6e4d7c763163b90e3704d1c29ae5ac72c8801f
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-12-09 18:06:06 +0800

    fix --serve-artifacts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ LISTENING_PORT=${LISTENING_PORT:-5000}
 if [ $# -eq 0 ]; then
   mkdir -p $ARTIFACT_DESTINATION_ROOT
   mlflow server --host 0.0.0.0 --port ${LISTENING_PORT} \
-    -serve-artifacts --artifacts-destination $ARTIFACTS_DESTINATION \
+    --serve-artifacts --artifacts-destination $ARTIFACTS_DESTINATION \
     --backend-store-uri ${BACKEND_STORE_URI}
 else
   eval "$@"
