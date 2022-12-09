b440c8734f0fb17c52247a2ca67b14a1c4bd53a6
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-12-09 17:30:30 +0800

    make mlflow serve artifacts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -eo pipefail
 
-DEFAULT_ARTIFACT_ROOT=${DEFAULT_ARTIFACT_ROOT:-"/data/artifact"}
+ARTIFACT_DESTINATION_ROOT=${ARTIFACT_DESTINATION_ROOT:-"/data/artifact"}
 BACKEND_STORE_URI=${BACKEND_STORE_URI:-"sqlite:////data/mlflow.db"}
 LISTENING_PORT=${LISTENING_PORT:-5000}
 
 if [ $# -eq 0 ]; then
   mlflow server --host 0.0.0.0 --port ${LISTENING_PORT} \
-    --default-artifact-root $DEFAULT_ARTIFACT_ROOT \
+    --artifact-destination-root $ARTIFACT_DESTINATION_ROOT \
     --backend-store-uri ${BACKEND_STORE_URI}
 else
   eval "$@"
